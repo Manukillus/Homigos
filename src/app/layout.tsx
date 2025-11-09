706cc0a5dd5150df2309a4f211d7be632d6108e5
NIRNAY <nirnay2847@gmail.com>
2025-11-09 13:39:56 +0000

    Make it dark themed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
         <FirebaseClientProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
