332daa2cbb339b645b7a0036ab24790b7b4a5d4c
NIRNAY <nirnay2847@gmail.com>
2025-11-09 11:24:34 +0000

    Make the size fit to screen (_for element div_)

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -3,11 +3,13 @@ import Header from '@/components/header';
 
 export default function BillingPage() {
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Header title="Billing" />
-      <div className="p-6">
-        <BillsTracker />
+      <div className="flex-1 p-6 overflow-y-auto">
+        <div className="container mx-auto">
+          <BillsTracker />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
