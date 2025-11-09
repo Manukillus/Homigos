82d50f9a06de8a9c067fe6a8079b4c5458106226
NIRNAY <nirnay2847@gmail.com>
2025-11-09 13:41:51 +0000

    Make the website Light themed but light coloured grey and white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
         <FirebaseClientProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
